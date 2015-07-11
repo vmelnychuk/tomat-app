7483d403cd78e98d214d040796de44c2ce7db81f
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-07-11 14:24:28 +0300

    add javafx basic stage

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,31 @@
 package app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private final String APP_TITLE = "Tomat";
+    private final double SCENE_WIDTH = 200;
+    private final double SCENE_HEIGHT = 100;
+    private final String OK_BUTTON_TITLE = "OK";
+    private Button okButton;
+    private StackPane layout;
+    private Scene scene;
     public static void main(String[] args) {
-        System.out.println("Tomat App");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle(APP_TITLE);
+        okButton = new Button(OK_BUTTON_TITLE);
+        layout = new StackPane();
+        layout.getChildren().add(okButton);
+        scene = new Scene(layout, SCENE_WIDTH, SCENE_HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
